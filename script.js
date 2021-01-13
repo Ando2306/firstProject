9fb8c188a82c178587f8b01b4a977764c061327f
Ando2306 <andobaloyan08@gmail.com>
2021-01-13 14:35:20 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,721 @@
+// number
+// string
+// boolean
+// null
+// undefined
+// symbol
+
+
+// object
+
+// var,
+// let, const
+// ES6 standart  2015 year
+
+
+
+
+
+// let anun = 1000.45
+// typeof
+// alert(num)
+// document.write(anun + '<br>')
+// document.write(typeof anun)
+
+
+
+// let str = 'abcd 45 345 2@@'
+// document.write(str + '<br>')
+// document.write(typeof str)
+
+
+
+// console.log(1455456)
+
+
+
+// boolean
+// true, false
+
+
+
+// // undefined
+// let a;
+// alert(a)
+
+
+
+
+// let a = null  //0
+// alert(a)
+
+
+
+// let a=5;
+// let b=12;
+// let s = b % a;
+// a++
+// b--
+// // alert(s)
+// alert(b)
+// +
+// -
+// *
+// /
+// %
+// ++ ինկրեմենտ
+// -- դեկրեմենտ
+
+
+// let sec = 24 * 60 * 60
+
+// document.write(259200 + '<br>')
+// document.write(sec)
+
+// let a=8
+// let b=8
+// let s=8*8
+// alert(s)
+
+
+
+// let s = 'abc'
+// let s1 = 'abc'
+// let s2 = 'def'
+// let s3 = s1 + s2
+// alert(s3)
+// let num = 10
+// let res = s + num //konkatenacia
+// alert(res)
+// alert(typeof res)
+
+
+
+
+// let str = 'abc'
+// let str1 = '20'
+// let num = 10
+// let res = str1 * num
+// alert(res)
+// // NaN---Not a Number
+
+
+
+
+// let a = Math.pow(78945457845, 50000)
+// let num = -45 / 0
+
+// alert(num)
+// console.log(a)
+
+
+
+
+// let a = 10;
+// a = a + 2
+// a += 2
+
+// a = a - 2
+// a -= 2
+// a = a * 2
+// a *= 2
+// a = a / 2
+// a /= 2
+
+// let b = 10
+// alert(a)
+
+
+// let n = '45'
+// alert(typeof n)
+// n = +n
+// alert(typeof n)
+
+
+
+// prompt
+// let num = 1452
+// let num1 = +prompt('nermutsel tivy')
+// let num2 = +prompt('nermutsel tivy')
+// // num1 = Number(num1)
+// // num2 = Number(num2)
+// alert(num1 + num2)
+
+
+
+
+
+// confirm
+// let t = confirm('Duq 20 tarekan eq?')
+// alert(t)
+
+
+
+
+
+// if, if-else, switch-case
+// let a = 5;
+// if(a > 13){
+// 	a = 10
+// }
+// else{
+// 	a = 0
+// }
+// alert(a)
+
+
+
+
+// let a = 10
+// if(a == 5){
+// 	alert(a)
+// }else if(a == 8){
+// 	alert(a)
+// }else if(a == 2){
+// 	alert(a)
+// }else{
+// 	a = 0
+// 	alert(a)
+// }
+
+
+
+
+// let lang = 'am'
+// switch(lang){
+// 	case 'am':
+// 	alert(lang);
+// 	break; 
+// 	case 'en':
+// 	alert(lang);
+// 	break; 
+// 	default:
+// 	alert(false)
+// 	break;
+// }
+
+
+
+
+
+
+// >, <, ==, !=, >=, <=, ===, !==
+
+// let a = '5'
+// let b = '10'
+// if(a != b){
+// 	alert(a, b)
+// }
+// else{
+// 	alert('error')
+// }
+
+
+
+
+// // ||--OR;  &&--AND
+// let a = 5, b = 10;
+// if(a > 7 || b < 20){
+// 	alert(true)
+// }
+// else{
+// 	alert(false)
+// }
+
+
+// 1․Եթե a փոփոխականը մեծ կամ հավասար է 3-ից, ապա արտածել   ՛՛ճիշտ է՛՛, հակառակ դեպքում արտածել ՛՛սխալ է՛՛։
+
+// let a=5
+
+// if( a<1){
+// alert(true)
+// } 
+// else{
+// 	alert(false)
+// }
+
+
+// 2․ Եթե a փոփոխականը հավասար  չէ  1-ի, ապա արտածել   ՛՛ճիշտ է՛՛, հակառակ դեպքում արտածել ՛՛սխալ է՛՛։
+
+// let a=6
+
+// if( a != 1){
+// alert(true)
+// }
+
+
+// else{
+// 	alert(false)
+// }
+
+
+// 3․ Եթե a փոփոխականը հավասար է ՛4՛-ի և արժեքով և տիպով ապա արտածել   ՛՛ճիշտ է՛՛, հակառակ դեպքում արտածել ՛՛սխալ է՛՛։
+
+// let a=6
+// let num=4
+
+// if (a!== num)
+// alert (true) 
+
+// else{
+//  alert(false)
+// }
+
+// 4․ Եթե test փոփոխականը հավասար է true, ապա արտածել   ՛՛ճիշտ է՛՛, հակառակ դեպքում արտածել ՛՛սխալ է՛՛։
+
+// let test = true
+
+// if (test=true){
+// 	alert(true)
+// }
+
+// else{
+// 	alert (false)
+// }
+
+
+
+// 5. Եթե a փոփոխականը մեծ է 1-ից, և փոքր է 10-ից, ապա արտածել   ՛՛ճիշտ է՛՛, հակառակ դեպքում արտածել ՛՛սխալ է՛՛։
+
+// let a=8
+
+// if (a>1 && a<10){
+// 	alert (true)
+// }
+
+// else{
+// 	alert (false)
+// }
+            
+            // <tnayin>
+// 6․ Եթե a փոփոխականը մեծ է 7-ից, կամ փոքր հավասար է 15-ից, ապա արտածել   ՛՛ճիշտ է՛՛, հակառակ դեպքում արտածել ՛՛սխալ է՛՛։
+
+// let a=7, b = 10;
+// if(a > 7 || b <= 15){
+// 	alert(true)
+// }
+// else{
+// 	alert(false)
+// }
+
+// 7․day փոփոխականը կարող է ընդունել արժեք 1-ից 31 ինտերվալից(ամսվա օրերը)։ Որոշել թե որ տասնամյակի մեջ է գտնվում day-ը։
+
+// let day = +prompt('nermutsel tivy')
+// // day = Number(day)
+// alert(typeof day)
+
+// if(day>0 && day<10){
+// 	alert ('gtnvum e arajin tasnyakum') 
+// }
+
+// else if (day>=10 && day<20){
+// 	alert ('gtnvum e erkrord tasnyakum') 
+// }
+
+// else if (day>=20 && day<31){
+// 	alert ('gtnvum e errord tasnyakum') 
+// }
+// else{
+// 	alert('error')
+// }
+
+
+
+// 10․ if-else –ի միջոցով հաշվել նախորոք արժեքավորված a, b, c թվերից մեծագույնը։
+
+// let  a= + prompt ('nermutsel a')
+// let  b= +prompt ('nermutsel b')
+// let  c= +prompt ('nermutsel c')
+
+// // alert(typeof a)
+
+// if(a>b && a>c){
+// 	alert(a)
+// }
+// else if(b>a && b>c){
+// 	alert(b)
+// }
+// else if(c>a && c>b){
+// 	alert(c)
+// }
+
+
+
+
+
+
+// Ցիկլեր
+// while
+
+// let a = 5
+// while(a < 20){
+// 	document.write(a + '<br>')
+// 	a++
+// }
+
+
+// do-while
+
+// let a = 0
+// do{
+// 	document.write(a + '<br>')
+// 	a++
+// }while(a < 20)
+
+
+// for
+// for(skzbnakan hramanner; cikli avartman paymany; cikli ancman hramannery){
+// 	....
+// 	...
+// 	....
+// }
+
+
+
+// for(let a = 0; a < 20; a++){
+// 	document.write(a + '<br>')
+// }
+
+
+
+// 1․ Արտածել 1-50 թվերի սյունակը, while-ի և for-ի միջոցով։
+
+// let a=1
+// while(a<51){
+// document.write(a+ '<br>')
+// a++	
+// }
+
+
+// for(let a = 1; a<51; a++){
+// 	document.write(a+'<br>')
+// }
+
+
+// 2․ Արտածել 13-37 թվերի սյունակը, while-ի և for-ի միջոցով։
+
+// let a=13
+// while(a<38){
+// 	document.write(a+ '<br>')
+// 	// a++
+// 	a = a + 2;
+// }
+
+// for(let a = 13; a < 51; a++){
+// 	document.write(a+'<br>')
+// }
+
+
+
+// 3․ Արտածել 1-100 թվերի սյունակը 2-ական քայլով, while-ի և for-ի միջոցով։
+
+// for(let a=1; a<101; a=a+2){
+// 	document.write(a+'<br>')
+// }
+
+
+
+// [1-10] tvery gumary
+// let s = 0
+// for(let a =1; a<10; a++){
+// 	s = s + a
+// }
+// console.log(s)
+
+
+// let p = 1
+// for(let a =1; a<=10; a++){
+// 	p = p * a
+// }
+// console.log(p)
+
+
+// for(let n = 1; n <= 50; n++){
+// 	if(n%2 != 0){
+// 		document.write(n+'<br>')
+// 	}
+// }
+
+
+      // <tnayin>
+      // 6. Հաշվել 1-10 միջակայքի թվերի գումարը:
+
+     //   let s = 0
+     //  for(let a =1; a<10; a++){
+	    // s = s + a
+     //    }
+     //  console.log(s)
+
+     
+
+     
+    // 7․ Հաշվել 1-5 միջակայքի զույգ թվերի գումարը։
+      
+      // let s = 0
+      // for(let a =1; a<=5; a++ ){
+      // 		if(a % 2 == 0){
+	    	// 	s = s + a
+      // 		}
+      //   }
+      // console.log(s)
+
+
+
+
+     // 8․ Հաշվել 1-5 միջակայքի կենտ թվերի գումարը։
+
+// let s=0
+// for(let a=1; a<=5; a++){
+// 	if (a % 2 != 0){
+// 		s=s+a
+// 	}
+// }
+// console.log(s)
+
+
+
+
+     // 9․ Հաշվել 1-100 միջակայքում բոլոր այն թվերի գումարը,
+     // որոնք առանց մնացորդի բաժանվում են 3-ի։
+
+//      s=0
+//     for(let a=3; a<101; a=a+3){
+//     	s = s + a
+// 	document.write(a+'<br>')
+//    }
+// 	console.log(s)
+
+// let s1 = 0
+// 	for(let i = 1; i <=100; i++){
+// 		if(i % 3 == 0){
+// 			s1 = s1 + i
+// 		}
+// 	}
+// console.log(s1)
+
+
+
+
+ // 10.Հաշվել 1-15 միջակայքի արտադրյալը։
+
+//  let p = 1
+// for(let a =1; a<=15; a++){
+// 	p = p * a
+// }
+// console.log(p)
+
+
+// 11․Հաշվել 1-20 միջակայքի  զույգ թվերի արտադրյալը։
+
+//  let p = 1
+// for(let a =1; a<=20; a++){
+// 	if(a % 2 == 0){
+// 		p = p * a
+// 	}
+// }
+
+// console.log(p)
+
+// 12․Հաշվել 1-20 միջակայքի  կենտ թվերի արտադրյալը։
+
+   // let p=1
+   // for(let a=1; a<=20; a++){
+   // 	if (a % 2 !=0){
+   // 		p = p * a
+   // 	}
+   // }
+   // console.log(p)
+
+
+
+
+
+
+// for(let i = 0; i < 10; i++){
+// 	console.log(i)
+// 	if(i == 5){
+// 		break
+// 	}
+// }
+
+// for(let i = 0; i < 10; i++){
+// 	if(i == 5){
+// 		continue;
+// 	}
+// 	console.log(i);
+// }
+
+
+
+// 5․Գտնել 100-400 միջակայքի բոլոր այն թվերը, որոնք առանց մնացորդի բաժանվում են 13, կամ 27-ի։
+
+
+
+// for(let a=100; a<=400; a++ ){
+// 	if (a%13==0 || a%27==0   ||a%36==0 )
+// 	document.write(a+'<br>')
+// }
+// console.log(a)
+
+
+
+
+// ․Գտնել և տպել 58 թվի բոլոր բաժանարարները։
+
+// for (let a=1; a<58; a++){
+// 	if (58 % a == 0) 
+// 		document.write(a + '<br>')
+// }
+
+
+// Ներկառուցված ցիկլի օգնությամբ տպել․
+// 1
+// 22
+// 333
+// 4444
+// 55555
+
+
+// for(let i = 0; i < 5; i++){
+// 	for(let j = 0; j < 5; j++){
+// 		console.log(j)
+// 	}
+// }
+
+// for (let i = 1; i <= 5; i++){
+// 	for (let j = 0;  j < i; j++) {
+// 		document.write('*' )
+		
+// 	}
+// 	document.write('<br>')
+
+
+// }
+    // <tnayin>
+
+ // 1․Ցիկլի օգնությամբ գեներացնել ՛՛123456789101112՛՛ տողը և այն գրել str փոփոխականի մեջ։
+
+// let str = ''
+// for(let a=1; a<6;  a++ ){
+// 	document.write(a)
+// 	str=str+a
+// }
+
+// console.log(str)
+
+// let str1 = ''
+// for(let a=1; a<=12;  a++ ){
+//   document.write(a)
+//   str1=str1+a
+// }
+
+// console.log(str1)
+
+
+
+
+// 4․Ստեղծել 5 շարքից բաղկացած բուրգ, օգտագործելով տող և ցիկլ։
+
+// for (let i = 1; i <= 5; i++){
+// 	for (let j = 0;  j < i; j++) {
+// 		document.write('*' )
+		
+// 	}
+// 	document.write('<br>')
+// }
+
+
+
+// str=''
+// for(let a=1; a<6; a++){
+// 	str=str + a
+// 	document.write(str)
+// 	document.write('<br>')
+// }
+
+
+// 5․ prompt-ի միջոցով ներմուծել թիվ։ Եթե նրա մեջ կա ՛5՛, ապա արտածել true, հակառակ դեպքում՝ false։
+// console.log(a)
+// console.log(a.length)
+
+// 9856// abcde
+// 0123
+
+
+// let  a = prompt ('nermutsel a'), z = 0
+// for(let i = 0; i < a.length; i++){
+// 	if(a[i] == '5'){
+// 		z = 1
+// 	}
+// }
+// if(z == 0){
+// 	console.log(false)
+// }
+// else{
+// 	console.log(true)
+// }
+
+
+
+
+
+
+
+// Array
+// let a = 1; 
+// let i = 2;
+// let b = 3;
+// let c = 4;
+// let f = 5;
+// let d = 6;
+// let e = 7;
+// let o = 8;
+// let k = 9;
+// let l = 10;
+
+// let arr = [1, 2, 3, 4, 5, 6, 'abc', true]
+// // 0 1 2 3 4 5 
+// for(let i = 0; i < arr.length; i++){
+// 	console.log(arr[i])
+// }
+
+
+
+// 1․Տրված է զանգված 1, 2, 3, 4, 5 էլեմենտներով։ Ցիկլի օգնությամբ այն արտածել էկրանին։
+
+// let arr =[10,2, 'aa', 3,7,12, 85, 'a1', 'ccc', 45]
+// for(let i = 0; i < arr.length; i++ ) {
+// 	console.log(arr[i])
+// }
+
+// let arr1=[1,2,3,4,5]
+// for(let i = 0; i < arr1.length; i++ ) {
+//   console.log(arr1[i])
+// }
+
+
+
+// console.log(arr[arr.length - 1])
+// console.log(arr.length) //10
+
+
+// let arr = [10, 2, 3, 4, 5]
+// // console.log(arr)
+// let s=0
+// for(let i=0; i<arr.length; i++){
+// 	// console.log(arr[i]
+//   // console.log(i)
+// 	// console.log(i,arr[i])
+// 	// s=s+arr[i]
+// }
+// document.write(s)
+
+
+
+// let a = 5, b = 10;
+// if(a > 7 || !!!(b < 20)){
+//   alert(true)
+// }
+// else{
+//   alert(false)
+// }
